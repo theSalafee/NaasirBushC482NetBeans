979f999b1bf519f9632b05cabacd08cce966498f
Naasir Salafee <naasirbush@icloud.com>
2019-09-09 20:17:50 -0700

    Added navigation functionality to add/cancel part

--- src/View_Controller/ModifyPartController.java
+++ src/View_Controller/ModifyPartController.java
@@ -5,14 +5,20 @@
  */
 package View_Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -63,7 +69,14 @@ public class ModifyPartController implements Initializable {
     }
 
     @FXML
-    private void cancelHandler(ActionEvent event) {
+    private void cancelHandler(ActionEvent event) throws IOException {
+        
+        Parent cancelPartParent = FXMLLoader.load(getClass().getResource("/View_Controller/MainScreen.fxml"));
+        Scene cancelPartScene = new Scene(cancelPartParent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.hide();
+        app_stage.setScene(cancelPartScene);
+        app_stage.show();
     }
 
     @FXML
