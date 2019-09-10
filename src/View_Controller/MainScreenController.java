321331820387ed5386232239808f009723538539
Naasir Salafee <naasirbush@icloud.com>
2019-09-09 20:04:04 -0700

    commiting to github

--- src/View_Controller/MainScreenController.java
+++ src/View_Controller/MainScreenController.java
@@ -5,18 +5,24 @@
  */
 package View_Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
+import javafx.scene.Node;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+///
 
 /**
  * FXML Controller class
@@ -55,6 +61,7 @@ public class MainScreenController implements Initializable {
     private Button productSearchBtn;
     @FXML
     private TextField searchProduct;
+    private Object FXMLoader;
 
     /**
      * Initializes the controller class.
@@ -73,12 +80,25 @@ public class MainScreenController implements Initializable {
     }
 
     @FXML
-    private void modifyPartHandler(ActionEvent event) {
+    private void modifyPartHandler(ActionEvent event) throws IOException {
+        
+        Parent modifyPartParent = FXMLLoader.load(getClass().getResource("/View_Controller/ModifyPart.fxml"));
+        Scene modifyPartScene = new Scene(modifyPartParent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.hide();
+        app_stage.setScene(modifyPartScene);
+        app_stage.show();
     }
 
     @FXML
-    private void addPartHandler(ActionEvent event) {
-//       
+    private void addPartHandler(ActionEvent event) throws IOException {
+        
+        Parent addPartParent = FXMLLoader.load(getClass().getResource("/View_Controller/AddPart.fxml"));
+        Scene addPartScene = new Scene(addPartParent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.hide();
+        app_stage.setScene(addPartScene);
+        app_stage.show();
         
     }
 
@@ -88,6 +108,8 @@ public class MainScreenController implements Initializable {
 
     @FXML
     private void modifyProductHandler(ActionEvent event) {
+        
+    
     }
 
     @FXML
