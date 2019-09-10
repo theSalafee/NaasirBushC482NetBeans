272284bbbf3177efd6ee1d150bdb6f1a071747aa
Naasir Salafee <naasirbush@icloud.com>
2019-09-09 20:55:35 -0700

    Added navigation functionality to add/cancel part

--- src/naasirbushc482/NaasirBushC482.java
+++ src/naasirbushc482/NaasirBushC482.java
@@ -19,7 +19,7 @@ public class NaasirBushC482 extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/View_Controller/MainScreen.fxml"));
         
         Scene scene = new Scene(root);
         
