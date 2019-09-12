86d661c318765590473b097b0627b7c6dff3bb2b
Naasir Salafee <naasirbush@icloud.com>
2019-09-11 19:30:48 -0700

    Added cancel functionality

---
+++ src/View_Controller/ModifyProductController.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View_Controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author naasirbush
+ */
+public class ModifyProductController implements Initializable {
+
+    @FXML
+    private TextField addPartID;
+    @FXML
+    private TextField addPartName;
+    @FXML
+    private TextField addPartinventory;
+    @FXML
+    private TextField addPartPrice;
+    @FXML
+    private TextField addPartMin;
+    @FXML
+    private TextField addPartMax;
+    @FXML
+    private Button cancelBtn;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+    } 
+    
+    @FXML
+    private void cancelHandler(ActionEvent event) throws IOException {
+        
+        Parent cancelModifyProductParent = FXMLLoader.load(getClass().getResource("/View_Controller/MainScreen.fxml"));
+        Scene cancelModifyProductScene = new Scene(cancelModifyProductParent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.hide();
+        app_stage.setScene(cancelModifyProductScene);
+        app_stage.show();
+    }
+    
+    
+    
+}
